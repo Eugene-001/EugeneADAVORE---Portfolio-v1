734cdc624a2f39922c704cd8beb2bbabd9b62e8f
Eugene-001 <your-github-email@example.com>
2026-05-03 18:31:43 +0100

    Developing Shelf21 Page.

--- src/Pages/Shelf21Details.jsx
+++ src/Pages/Shelf21Details.jsx
@@ -1,16 +1,139 @@
-import { useParams } from "react"
-import { useNavigation } from "react-router-dom"
+import CaseStudy from "../Components/CaseStudy";
 
 
 function Shelf21Details() {
 
-    const { id } = useParams();
-    const navigation = useNavigation();
-
 
     return (
         <div>
+            <div className="top-section">
+                <div className="slice-left">
+                    <div className="header-year">
+                        <div className="header">
+                            SHELF 21
+                        </div>
 
+                        <div className="year">
+                            PRODUCT DESIGN | 2025
+                        </div>
+                    </div>
+
+                    <div className="slice-description">
+                       Shelf 21 is an all-in-one school operations and communication platform that connects administrators, teachers, parents, and students eliminating fragmented tools and improving visibility across academic and operational workflows.
+                    </div>
+                </div>
+
+                <div className="slice-right">
+                    <div className="video-layer">
+                        <video src="/Media/shelf1.mov" autoPlay muted loop />
+                    </div>
+                </div>
+            </div>
+        
+            <div className="image-display">
+                <img src="/Media/01.svg" alt="" />
+                <img src="/Media/02.svg" alt="" />
+                <img src="/Media/03.svg" alt="" />
+                <img src="/Media/04.svg" alt="" />
+           </div>
+
+           <div className="the-problem">
+            <div className="left-problem">
+                <div className="problem-header">
+                    THE PROBLEM
+                </div>
+
+                <div className="problem-body">
+                    Schools and organizations rely on multiple disconnected tools to manage communication, operations, and records. This fragmentation leads to: Lost or hard-to-find information, Inefficient workflows across teams, Poor visibility into student performance ,Weak communication between schools and parents
+                </div>
+            </div>
+
+            <div className="right-problem">
+                <video src="/Media/shelf2.mov" autoPlay muted></video>
+            </div>
+           </div>
+
+           <div className="thesolution">
+                <div className="solution-left">
+                    <div className="head-solution">
+                        THE SOLUTION
+                    </div>
+
+                    <div className="body-solution">
+                        Shelf 21 was designed as a unified, role-driven platform that consolidates communication, academic workflows, and operational tools into a single system. By integrating key functions and structuring the experience around distinct user roles, the product ensures that each user interacts with relevant information and actions, reducing complexity while improving efficiency, clarity, and overall usability.
+                    </div>
+                </div>
+
+                <div className="solution-right">
+                    <img src="/Media/image 59.svg" alt="" />
+                </div>
+           </div>
+
+           <div className="new-purple">
+                <div className="purp-left">
+                    <div className="web-img">
+                        <video src="/Media/side.mp4" alt="" autoPlay muted/>
+                    </div>
+
+                    <div className="purple-text">
+                        <div className="pur-header">
+                            OPERATIONAL EFFIICIENCY THROUGH SYSTEM CONSOLIDATION.
+                        </div>
+
+                        <div className="body">
+                            The system was designed Shelf 21 as a centralized system to eliminate the need for multiple disconnected tools, improving operational efficiency and reducing friction in day-to-day school activities.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="purp-right">
+                    <div className="web-img">
+                        <video src="/Media/side2.mp4" alt="" autoPlay muted/>
+                    </div>
+
+                    <div className="purple-text">
+                        <div className="pur-header">
+                            BOOSTING USER ADOPTION WITH ROLE BASED EXPERIENCES
+                        </div>
+
+                        <div className="body">
+                            The system was designed Shelf 21 as a centralized system to eliminate the need for multiple disconnected tools, improving operational efficiency and reducing friction in day-to-day school activities.
+                        </div>
+                    </div>
+                </div>
+           </div>
+
+           <div className="product-scrope">
+                <div className="scopeleft">
+                    <div className="scope1">
+                        <div className="scopehead">
+                            Product Scope
+                        </div>
+
+                        <div className="scopebody">
+                            Shelf 21 was designed as a comprehensive platform that supports both operational and academic workflows within schools and organizations. The product brings together multiple core functions including communication, task management, performance tracking, academic planning, and reporting into a unified system. 
+
+Key capabilities span across internal messaging (chat and announcements), structured task and workflow management, student performance reporting, examination management, knowledge sharing, and administrative operations such as staff management and inventory tracking. By integrating these modules into a single platform, the product enables seamless coordination between stakeholders and ensures that critical information is accessible, organized, and actionable across the system.
+                        </div>
+                    </div>
+
+                    <div className="scope1">
+                        <div className="scopehead">
+                            Product Scope
+                        </div>
+
+                        <div className="scopebody">
+                            Shelf 21 was designed as a comprehensive platform that supports both operational and academic workflows within schools and organizations. The product brings together multiple core functions including communication, task management, performance tracking, academic planning, and reporting into a unified system. 
+
+Key capabilities span across internal messaging (chat and announcements), structured task and workflow management, student performance reporting, examination management, knowledge sharing, and administrative operations such as staff management and inventory tracking. By integrating these modules into a single platform, the product enables seamless coordination between stakeholders and ensures that critical information is accessible, organized, and actionable across the system.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="scoperight">
+                    <img src="/Media/scope.svg" alt="" />
+                </div>
+           </div>
         </div>
     )
 }
